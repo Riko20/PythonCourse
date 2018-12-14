db0e7af859a154aa2bdf3b4d774e71485ebfd23b
Kostiantyn Chichko <dragonagek@gmail.com>
2018-12-14 14:15:29 +0200

    Job is done

--- Homework3.py
+++ Homework3.py
@@ -97,14 +97,16 @@ print(convert(tup))
 #  1) it has to contain only alphanumerical characters (a−z, A−Z, 0−9);
 #  2) there should be an even number of letters;
 #  3) there should be an odd number of digits.
-inp = input()
 
 def set_pas(inp):
     digits = []
     lets = []
     prescr = [',', '.', '?', '/']
+
     for elem in inp:
-        if elem not in prescr:
+        if elem in prescr:
+            break
+        else:
             if elem.isdigit():
                 digits.append(elem)
             if elem.isalpha():
@@ -112,18 +114,35 @@ def set_pas(inp):
 
     if len(digits) % 2 == 0 :
         print("There should be odd number of digits. Please change password")
+        return '0'
     if len(lets) % 2 != 0:
         print("there should be even number of letters. Please change password")
+        return '0'
     else:
         print("Password correct")
         return inp
 
-set_pas(inp)
+
 
 # You are given a string S consisting of N characters. String S can be divided into words by splitting it at, and removing, the spaces.
 # The goal is to choose the longest word that is a valid password.
 # You can assume that if there are K spaces in string S then there are exactly K + 1 words.
-#
+example_string = "test 5 a0A pass007 ?xy1"
+
+def fing_the_longest(string):
+    b = []
+    a = string.split()
+    for words in a:
+        b.append(set_pas(words))
+
+    print("The longest one: ", max(b, key = len))
+
+
+fing_the_longest(example_string)
+
+
+
+
 # For example, given "test 5 a0A pass007 ?xy1", there are five words and three of them are valid passwords: "5", "a0A" and "pass007".
 # Thus the longest password is "pass007" and its length is 7.
 # Note that neither "test" nor "?xy1" is a valid password, because "?" is not an alphanumerical character and "test" contains an even number of digits (zero).
