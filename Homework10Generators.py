248d440c2ccea9542709934841c9f5347e539f57
Kostiantyn Chichko <dragonagek@gmail.com>
2019-02-23 15:32:46 +0200

    Coroutine has been constructed. Don`t know how to start reading file from tail.

--- Homework10Generators.py
+++ Homework10Generators.py
@@ -24,3 +24,74 @@ def sort_string():
             print(i)
 
 sort_string()
+
+# Задача-2 (оригинальный вариант и его делать не обязательно):
+# представим есть файл с логами, его нужно бессконечно контролировать
+# на предмет возникнования заданных сигнатур.
+#
+# Необходимо реализовать пайплайн из корутин, который подключается к существующему файлу
+# по принципу команды tail, переключается в самый конец файла и с этого момента начинает следить
+# за его наполнением, и в случае возникнования запиcей, сигнатуры которых мы отслеживаем -
+# печатать результат
+#
+# Архитектура пайплайна
+
+#                    --------
+#                   /- grep -\
+# dispenser(file) <- - grep - -> pprint
+#                   \- grep -/
+#                    --------
+
+# Структура пайплайна:
+# ```
+import os
+import io
+
+def corout(func):
+    def wrapper(*args, **kwargs):
+        c = func(*args, **kwargs)
+        c.send(None)
+        return c
+    return wrapper
+
+
+@corout
+def grep(sign, func):
+    while True:
+        x = yield
+        if sign in x:
+            print(type(func))
+            func.send(sign)
+
+
+
+@corout
+def printer():
+    while True:
+        mess = yield
+        print(mess)
+
+
+@corout
+def dispenser(greppe):
+    while True:
+        x = yield
+        for grepp in greppe:
+            grepp.send(x)
+
+
+
+f_open = open('C:/Users/Dragonagek/Desktop/PythonCourse/text1.txt')
+
+argum = [grep('Dave', printer()), grep('Martin', printer()), grep('Aloxa', printer())]
+disp = dispenser(argum)
+
+
+def follow(f_open, disp):
+    while True:
+        rl = f_open.readline()
+        if not rl:
+            break
+        disp.send(rl)
+
+follow(f_open, disp)
