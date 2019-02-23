7ec2e29576e9e3da8e2791e84986859efc3bee3f
Kostiantyn Chichko <dragonagek@gmail.com>
2019-02-23 15:27:22 +0200

    Descriptor Ingeger has been constructed

--- DescriptorsHomework9.py
+++ DescriptorsHomework9.py
@@ -54,3 +54,34 @@ wooo = MyClass()
 
 assert id(myclass) == id(wooo)
 
+# Задача-3
+# реализовать дескриптор IngegerField(), который будет хранить уникальные
+# состояния для каждого класса где он объявлен
+
+class IngegerField(object):
+
+    def __init__(self):
+        self.conditions = WeakKeyDictionary
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+
+        return self.conditions[instance]
+
+    def __set__(self, instance, value):
+        self.conditions[instance] = value
+
+
+class Data:
+    desc = IngegerField()
+
+
+data_raw = Data()
+new_data_raw = Data()
+
+data_raw.number = 10
+
+new_data_raw.number = 20
+
+assert data_raw.number != new_data_raw.number
